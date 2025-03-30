c36d7424ade08e9945ed6438d064825ce51dcdf5
jpitts1 <jpitts1@guilford.edu>
2025-03-29 23:00:21 -0400

    Updates

---
+++ testscript.js
@@ -0,0 +1,5 @@
+myitem.addEventListener("click", onClick); // Makes an action happen when you click the text.
+
+function onClick() {  // When you click the text, it turns it green.
+    myitem.style.color = "blue"
+}
